c19d309dc62daa15981376f2f66d4f2788f45761
Neti <98841356+Netidion@users.noreply.github.com>
2023-12-29 17:20:59 +0200

    Solution of basic assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+import { SuccessAlertComponent } from './successalert/successalert.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/successalert/successalert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './successalert.component.html',
+  styleUrl: './successalert.component.css'
+})
+export class SuccessAlertComponent {
+
+}
